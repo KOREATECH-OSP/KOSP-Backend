dd5ed12b8651f00f3f1b82d5c5d05e36cb5d2d7e
박성빈 <psb106305@gmail.com>
2025-10-27 16:35:45 +0900

    chore: Initialize Spring Boot project

---
+++ src/main/java/kr/ac/koreatech/sw/KOSPApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.koreatech.sw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KOSPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KOSPApplication.class, args);
+	}
+
+}
