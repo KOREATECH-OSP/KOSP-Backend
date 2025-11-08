5d3ef0a9de202ea06bd0d8f67724e986eb292c68
박성빈 <psb106305@gmail.com>
2025-11-09 04:06:35 +0900

    chore: Configure Gradle with version catalogs and update project structure

--- src/main/java/kr/ac/koreatech/sw/kosp/KOSPApplication.java
+++ src/main/java/kr/ac/koreatech/sw/kosp/KOSPApplication.java
@@ -1,4 +1,4 @@
-package kr.ac.koreatech.sw;
+package kr.ac.koreatech.sw.kosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
