e3191c23a89cfb866111e50e7295d7edde56054e
PARK TAEJIN <140797244+taejinn@users.noreply.github.com>
2025-12-01 20:49:34 +0900

    feat: swagger 작성

--- src/main/java/kr/ac/koreatech/sw/kosp/domain/auth/controller/AuthController.java
+++ src/main/java/kr/ac/koreatech/sw/kosp/domain/auth/controller/AuthController.java
@@ -1,17 +1,10 @@
 package kr.ac.koreatech.sw.kosp.domain.auth.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.Valid;
+import kr.ac.koreatech.sw.kosp.domain.auth.api.AuthApi;
 import kr.ac.koreatech.sw.kosp.domain.auth.dto.request.LoginRequest;
 import kr.ac.koreatech.sw.kosp.domain.auth.dto.response.AuthMeResponse;
 import kr.ac.koreatech.sw.kosp.domain.auth.service.AuthService;
@@ -20,17 +13,14 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-@RequestMapping("/v1/auth")
 @RequiredArgsConstructor
-@Tag(name = "Auth", description = "인증 관련 API")
-public class AuthController {
+public class AuthController implements AuthApi {
 
     private final AuthService authService;
 
-    @PostMapping("/login")
-    @Operation(summary = "일반 로그인", description = "이메일과 비밀번호로 로그인")
+    @Override
     public ResponseEntity<Void> login(
-        @RequestBody @Valid LoginRequest request,
+        LoginRequest request,
         HttpServletRequest servletRequest,
         HttpServletResponse servletResponse
     ) {
@@ -39,18 +29,15 @@ public class AuthController {
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/logout")
-    @Operation(summary = "로그아웃", description = "현재 로그인한 사용자 로그아웃")
+    @Override
     public ResponseEntity<Void> logout(HttpServletRequest request) {
         authService.logout(request);
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/me")
-    @Operation(summary = "내 정보 조회", description = "현재 로그인한 사용자 정보 조회")
+    @Override
     public ResponseEntity<AuthMeResponse> getMyInfo() {
-        AuthMeResponse response = authService.getUserInfo();
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(authService.getUserInfo());
     }
 
 }
