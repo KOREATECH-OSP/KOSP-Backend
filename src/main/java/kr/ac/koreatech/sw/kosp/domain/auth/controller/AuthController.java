1120ccab5191b74c029cba75fc66e5396f7df5f9
박성빈 <psb106305@gmail.com>
2025-12-21 19:28:39 +0900

    feat: Add endpoints for user signup and authentication API
    - Introduced `/v1/users/signup` in `UserController` for user registration.
    - Added `/v1/auth/login`, `/v1/auth/logout`, and `/v1/auth/me` in `AuthController`.
    - Enhanced validation with `@Valid` and streamlined request mappings.

--- src/main/java/kr/ac/koreatech/sw/kosp/domain/auth/controller/AuthController.java
+++ src/main/java/kr/ac/koreatech/sw/kosp/domain/auth/controller/AuthController.java
@@ -1,28 +1,32 @@
 package kr.ac.koreatech.sw.kosp.domain.auth.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import kr.ac.koreatech.sw.kosp.domain.auth.api.AuthApi;
 import kr.ac.koreatech.sw.kosp.domain.auth.dto.request.LoginRequest;
 import kr.ac.koreatech.sw.kosp.domain.auth.dto.response.AuthMeResponse;
-import kr.ac.koreatech.sw.kosp.domain.auth.dto.response.OAuth2Response;
 import kr.ac.koreatech.sw.kosp.domain.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @RestController
+@RequestMapping("/v1/auth")
 @RequiredArgsConstructor
 public class AuthController implements AuthApi {
 
     private final AuthService authService;
 
     @Override
+    @PostMapping("/login")
     public ResponseEntity<Void> login(
-        LoginRequest request,
+        @RequestBody @Valid LoginRequest request,
         HttpServletRequest servletRequest,
         HttpServletResponse servletResponse
     ) {
@@ -32,24 +36,16 @@ public class AuthController implements AuthApi {
     }
 
     @Override
+    @PostMapping("/logout")
     public ResponseEntity<Void> logout(HttpServletRequest request) {
         authService.logout(request);
         return ResponseEntity.ok().build();
     }
 
     @Override
+    @GetMapping("/me")
     public ResponseEntity<AuthMeResponse> getMyInfo() {
         return ResponseEntity.ok(authService.getUserInfo());
-    }
-
-    @Override
-    public ResponseEntity<OAuth2Response> oAuth2ResultHandler(HttpServletRequest request) {
-        boolean isNew = (boolean)request.getAttribute("isNew");
-        Long githubId = (Long)request.getAttribute("githubId");
-
-        // TODO githubId로 User를 조회한 다음, 존재하는지 확인. 없으면 isNew는 true이고, 있으면 false임.
-
-        return ResponseEntity.ok(new OAuth2Response(isNew, githubId));
     }
 
 }
