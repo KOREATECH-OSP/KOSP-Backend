0326c63e522a0238f4ed3f9223feb8607ee7bb69
박성빈 <psb106305@gmail.com>
2025-12-25 17:04:32 +0900

    refactor: add `findAll` method to BoardRepository and remove unused `existsById` method

--- src/main/java/kr/ac/koreatech/sw/kosp/domain/community/board/repository/BoardRepository.java
+++ src/main/java/kr/ac/koreatech/sw/kosp/domain/community/board/repository/BoardRepository.java
@@ -1,5 +1,6 @@
 package kr.ac.koreatech.sw.kosp.domain.community.board.repository;
 
+import java.util.List;
 import java.util.Optional;
 import kr.ac.koreatech.sw.kosp.domain.community.board.model.Board;
 import kr.ac.koreatech.sw.kosp.global.exception.ExceptionMessage;
@@ -8,9 +9,9 @@ import org.springframework.data.repository.Repository;
 
 public interface BoardRepository extends Repository<Board, Long> {
 
-    Optional<Board> findById(Long id);
+    List<Board> findAll();
 
-    boolean existsById(Long id);
+    Optional<Board> findById(Long id);
 
     default Board getById(Long id) {
         return findById(id)
