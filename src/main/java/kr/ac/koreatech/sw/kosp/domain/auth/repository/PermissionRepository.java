3b53fc04ff3b49a93b3e0a320e94f0d9e60feea1
박성빈 <psb106305@gmail.com>
2026-01-04 15:33:39 +0900

    fix: test

--- src/main/java/kr/ac/koreatech/sw/kosp/domain/auth/repository/PermissionRepository.java
+++ src/main/java/kr/ac/koreatech/sw/kosp/domain/auth/repository/PermissionRepository.java
@@ -1,8 +1,10 @@
 package kr.ac.koreatech.sw.kosp.domain.auth.repository;
 
 import java.util.List;
-import kr.ac.koreatech.sw.kosp.domain.auth.model.Permission;
+
 import org.springframework.data.repository.Repository;
+
+import kr.ac.koreatech.sw.kosp.domain.auth.model.Permission;
 
 public interface PermissionRepository extends Repository<Permission, Long> {
     Permission save(Permission permission);
