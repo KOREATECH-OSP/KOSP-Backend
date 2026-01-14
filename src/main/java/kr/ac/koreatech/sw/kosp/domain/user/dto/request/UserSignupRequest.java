e77dea871dbbb8a6711fff1cbba939746f841f09
박성빈 <psb106305@gmail.com>
2026-01-15 00:34:31 +0900

    fix: 회원가입 토큰 위치 수정

--- src/main/java/kr/ac/koreatech/sw/kosp/domain/user/dto/request/UserSignupRequest.java
+++ src/main/java/kr/ac/koreatech/sw/kosp/domain/user/dto/request/UserSignupRequest.java
@@ -29,10 +29,6 @@ public record UserSignupRequest(
     @NotBlank(message = "비밀번호는 필수입니다.")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$", 
              message = "비밀번호는 영문, 숫자, 특수문자를 포함하여 8자 이상이어야 합니다.")
-    String password,
-
-    @Schema(description = "회원가입 토큰 (JWS)", example = "eyJ...", requiredMode = REQUIRED)
-    @NotBlank(message = "회원가입 토큰은 필수입니다.")
-    String signupToken
+    String password
 ) {
 }
