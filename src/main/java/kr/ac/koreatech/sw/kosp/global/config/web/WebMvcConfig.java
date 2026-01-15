1d12fd6384c531f3649b318e8e4912fd66935726
박성빈 <psb106305@gmail.com>
2026-01-15 11:08:46 +0900

    feature: Origin에서 ClientURL 파싱 추가

--- src/main/java/kr/ac/koreatech/sw/kosp/global/config/web/WebMvcConfig.java
+++ src/main/java/kr/ac/koreatech/sw/kosp/global/config/web/WebMvcConfig.java
@@ -8,6 +8,8 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import kr.ac.koreatech.sw.kosp.global.auth.resolver.JwtArgumentResolver;
+import kr.ac.koreatech.sw.kosp.global.host.ClientURLArgumentResolver;
+import kr.ac.koreatech.sw.kosp.global.host.ClientURLInterceptor;
 import kr.ac.koreatech.sw.kosp.global.host.ServerURLArgumentResolver;
 import kr.ac.koreatech.sw.kosp.global.host.ServerURLInterceptor;
 import lombok.RequiredArgsConstructor;
@@ -18,13 +20,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     private final AuthUserArgumentResolver authUserArgumentResolver;
     private final ServerURLArgumentResolver serverURLArgumentResolver;
+    private final ClientURLArgumentResolver clientURLArgumentResolver;
     private final ServerURLInterceptor serverURLInterceptor;
+    private final ClientURLInterceptor clientURLInterceptor;
     private final JwtArgumentResolver jwtArgumentResolver;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authUserArgumentResolver);
         resolvers.add(serverURLArgumentResolver);
+        resolvers.add(clientURLArgumentResolver);
         resolvers.add(jwtArgumentResolver);
     }
 
@@ -33,5 +38,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(serverURLInterceptor)
             .addPathPatterns("/**")
             .order(2);
+        registry.addInterceptor(clientURLInterceptor)
+            .addPathPatterns("/**")
+            .order(3);
     }
 }
