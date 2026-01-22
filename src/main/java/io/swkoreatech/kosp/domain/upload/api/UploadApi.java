6feab3bc3fe1d201c95704a6a37bf61754d0d92f
박성빈 <psb106305@gmail.com>
2026-01-22 12:07:09 +0900

    fix: file upload

--- src/main/java/io/swkoreatech/kosp/domain/upload/api/UploadApi.java
+++ src/main/java/io/swkoreatech/kosp/domain/upload/api/UploadApi.java
@@ -1,5 +1,12 @@
 package io.swkoreatech.kosp.domain.upload.api;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -7,11 +14,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swkoreatech.kosp.domain.upload.dto.response.FileResponse;
 import io.swkoreatech.kosp.domain.user.model.User;
 import io.swkoreatech.kosp.global.security.annotation.AuthUser;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 @Tag(name = "Upload", description = "파일 업로드 API")
 @RequestMapping("/v1/files")
@@ -22,10 +24,10 @@ public interface UploadApi {
         description = "S3에 파일을 업로드하고 첨부파일 정보를 반환합니다. 반환된 ID를 사용하여 게시글 작성 시 첨부할 수 있습니다."
     )
     @ApiResponse(responseCode = "200", description = "업로드 성공")
-    @PostMapping
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     ResponseEntity<FileResponse> uploadFile(
         @Parameter(description = "업로드할 파일", required = true)
-        @RequestParam("file") MultipartFile file,
+        @RequestPart("file") MultipartFile file,
         @AuthUser User user
     );
 }
