fea5a61ac516c762c6b5b562ce92ce2936c3ad27
박성빈 <psb106305@gmail.com>
2026-01-29 10:40:21 +0900

    debug(notification): add SSE subscription logging

--- backend/src/main/java/io/swkoreatech/kosp/domain/notification/controller/NotificationController.java
+++ backend/src/main/java/io/swkoreatech/kosp/domain/notification/controller/NotificationController.java
@@ -13,7 +13,9 @@ import io.swkoreatech.kosp.domain.notification.service.NotificationService;
 import io.swkoreatech.kosp.domain.user.model.User;
 import io.swkoreatech.kosp.global.security.annotation.Permit;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class NotificationController implements NotificationApi {
@@ -23,6 +25,12 @@ public class NotificationController implements NotificationApi {
     @Override
     @Permit(name = "notification:subscribe", description = "SSE 알림 구독")
     public SseEmitter subscribe(User user) {
+        if (user == null) {
+            log.warn("📢 [SSE] subscribe() called but user is null");
+        } else {
+            log.info("📢 [SSE] User {} (ID: {}) subscribing to notifications", 
+                user.getName(), user.getId());
+        }
         return notificationService.subscribe(user.getId());
     }
 
