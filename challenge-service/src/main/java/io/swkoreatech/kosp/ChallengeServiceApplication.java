c77f96db41309a889bb51e54006d256112029bfd
박성빈 <psb106305@gmail.com>
2026-02-01 14:56:30 +0900

    fix(challenge-service): ComponentScan 범위 제한하여 AuthService Bean 로드 방지
    - backend 전체 스캔 → 필요한 패키지만 명시적 스캔
    - scanBasePackages: challenge, domain.{challenge,user,github,point}, rabbitmq
    - @EnableJpaRepositories, @EntityScan 범위 명시
    - OAuth2 설정 없이도 실행 가능하도록 수정

--- challenge-service/src/main/java/io/swkoreatech/kosp/ChallengeServiceApplication.java
+++ challenge-service/src/main/java/io/swkoreatech/kosp/ChallengeServiceApplication.java
@@ -8,7 +8,25 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+    "io.swkoreatech.kosp.challenge",
+    "io.swkoreatech.kosp.domain.challenge",
+    "io.swkoreatech.kosp.domain.user",
+    "io.swkoreatech.kosp.domain.github",
+    "io.swkoreatech.kosp.domain.point",
+    "io.swkoreatech.kosp.infra.rabbitmq"
+})
+@EnableJpaRepositories(basePackages = {
+    "io.swkoreatech.kosp.domain.challenge.repository",
+    "io.swkoreatech.kosp.domain.user.repository",
+    "io.swkoreatech.kosp.domain.github.repository"
+})
+@EntityScan(basePackages = {
+    "io.swkoreatech.kosp.domain.challenge.model",
+    "io.swkoreatech.kosp.domain.user.model",
+    "io.swkoreatech.kosp.common.github.model",
+    "io.swkoreatech.kosp.common.model"
+})
 @EnableScheduling
 public class ChallengeServiceApplication {
     public static void main(String[] args) {
