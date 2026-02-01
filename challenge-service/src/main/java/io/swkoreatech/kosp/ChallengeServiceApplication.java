0f7c084af31f0e159b5684ff706352222c9c1515
박성빈 <psb106305@gmail.com>
2026-02-01 14:28:00 +0900

    chore(challenge-service): 설정 개선 및 의존성 추가
    - build.gradle.kts: bootRun에 시스템 프로퍼티 및 환경변수 전달
    - application.yml: Redis 설정 추가, Flyway 비활성화
    - ChallengeServiceApplication: Spring Data JPA 어노테이션 import 추가
    - 실행 환경 개선

--- challenge-service/src/main/java/io/swkoreatech/kosp/ChallengeServiceApplication.java
+++ challenge-service/src/main/java/io/swkoreatech/kosp/ChallengeServiceApplication.java
@@ -3,6 +3,9 @@ package io.swkoreatech.kosp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
