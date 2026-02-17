edd63e4512cffbd7ba8baa078649704d2743a97c
박성빈 <psb106305@gmail.com>
2026-02-17 17:31:32 +0900

    chore(rabbitmq): refactor config and extract queue name constants

--- rabbitmq/src/main/java/io/swkoreatech/kosp/infra/rabbitmq/constants/QueueNames.java
+++ rabbitmq/src/main/java/io/swkoreatech/kosp/infra/rabbitmq/constants/QueueNames.java
@@ -1,9 +1,14 @@
 package io.swkoreatech.kosp.infra.rabbitmq.constants;
 
 public final class QueueNames {
+
+    public static final String X_DEAD_LETTER_EXCHANGE = "x-dead-letter-exchange";
+    public static final String X_DEAD_LETTER_ROUTING_KEY = "x-dead-letter-routing-key";
+
     public static final String CHALLENGE_EVALUATION = "challenge-evaluation-queue";
     public static final String CHALLENGE_COMPLETED = "challenge-completed-queue";
     public static final String POINT_CHANGED = "point-changed-queue";
-    
-    private QueueNames() {}
+
+    private QueueNames() {
+    }
 }
