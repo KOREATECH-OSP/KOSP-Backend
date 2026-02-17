d4218c03515d2077ef726be791ca8ed5df389cd4
박성빈 <psb106305@gmail.com>
2026-02-17 22:18:42 +0900

    refactor: remove redis zset job queue infrastructure

--- common/src/main/java/io/swkoreatech/kosp/common/queue/JobQueueEntry.java
+++
@@ -1,3 +0,0 @@
-package io.swkoreatech.kosp.common.queue;
-
-public record JobQueueEntry(Long userId, String runId) {}
--- harvester/src/main/java/io/swkoreatech/kosp/trigger/UserIdProvider.java
+++
@@ -1,7 +0,0 @@
-package io.swkoreatech.kosp.trigger;
-
-import java.util.List;
-
-public interface UserIdProvider {
-    List<Long> findActiveUserIds();
-}
