0ac28c7635e39ee6f9e0df894d31c4be86690fd9
kang-junhyeong <kevin32529@gmail.com>
2026-07-19 22:23:41 +0900

    조직 등록 인서트 후 딜리트 문제 해결

--- common/src/main/java/io/swkoreatech/kosp/common/organization/repository/OrganizationMemberRepository.java
+++ common/src/main/java/io/swkoreatech/kosp/common/organization/repository/OrganizationMemberRepository.java
@@ -3,6 +3,8 @@ package io.swkoreatech.kosp.common.organization.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 import io.swkoreatech.kosp.common.exception.ExceptionMessage;
@@ -23,6 +25,8 @@ public interface OrganizationMemberRepository extends Repository<OrganizationMem
 
     List<OrganizationMember> findAllByGithubUserId(Long githubUserId);
 
+    @Modifying
+    @Query("DELETE FROM OrganizationMember m WHERE m.organization.id = :organizationId")
     void deleteAllByOrganizationId(Long organizationId);
 
     Optional<OrganizationMember> findByOrganizationIdAndUserId(Long organizationId, Long userId);
--- common/src/main/java/io/swkoreatech/kosp/common/organization/repository/OrganizationRepoRepository.java
+++ common/src/main/java/io/swkoreatech/kosp/common/organization/repository/OrganizationRepoRepository.java
@@ -2,6 +2,8 @@ package io.swkoreatech.kosp.common.organization.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 import io.swkoreatech.kosp.common.organization.model.OrganizationRepo;
@@ -12,5 +14,7 @@ public interface OrganizationRepoRepository extends Repository<OrganizationRepo,
 
     List<OrganizationRepo> findAllByOrganizationId(Long organizationId);
 
+    @Modifying
+    @Query("DELETE FROM OrganizationRepo r WHERE r.organization.id = :organizationId")
     void deleteAllByOrganizationId(Long organizationId);
 }
